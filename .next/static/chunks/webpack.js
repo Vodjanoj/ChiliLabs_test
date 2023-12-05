8acb92e7ebe9b53977df2b1d8ec40a495ce65b9e
vodjanoj <vladimirs.vodjanojs@gmail.com>
2023-12-05 20:31:17 +0200

    font fixing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1aac7e036cf083c6"; }
+/******/ 		__webpack_require__.h = function() { return "b8547c88c1d2c2bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.507a054040c2c604.hot-update.js
+++ .next/static/webpack/webpack.507a054040c2c604.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e050585238e02fc8"; }
+/******/ 	__webpack_require__.h = function() { return "b8547c88c1d2c2bb"; }
 /******/ }();
 /******/ 
 /******/ }
